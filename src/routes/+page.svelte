cd70b51ae279d168829987d002c7e0208f6f4a02
Andreas Gajdosik <andreas@gajdosik.org>
2025-03-04 17:55:38 +0100

    Add some server side data fetching

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<!-- +page.svelte -->
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	// SvelteKit will inject `data`, which includes `items` and possibly `error`.
+	export let data: PageData;
+
+	// Destructure so we don’t have to keep writing `data.items`
+	const { items = [], error } = data;
+</script>
+
+<h1>Faculty of Fine Arts</h1>
+
+{#if error}
+	<p style="color: red;">Failed to load data: {error}</p>
+{:else if items.length > 0}
+	<ul>
+		{#each items as item}
+			<li>
+				<strong>{item.title}</strong><br/>
+				<em>{item.keywords}</em>
+			</li>
+		{/each}
+	</ul>
+{:else}
+	<p>No items found.</p>
+{/if}
+
+<style>
+	/* Add any styling you need here */
+</style>

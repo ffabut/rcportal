1f8724ee55688bfcc901487036e687ef90a96bd8
Andreas Gajdosik <andreas@gajdosik.org>
2025-08-04 12:27:10 +0530

    Init header, init faculty logos

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,10 +7,26 @@ import { ResearchListComponent } from '../../components/research-list/research-l
   standalone: true,
   imports: [CommonModule, ResearchListComponent],
   template: `
-    <div class="home">
-      <h1>FFA Art Research Portal</h1>
-      <app-research-list></app-research-list>
-    </div>
+<header>
+  <a>FFA</a>
+  <a>Organigram</a>
+  <a>Research</a>
+  <a>Publishing</a>
+  <a>Gallery</a>
+  <a>MA's in Fine Arts and Design</a>
+  <a>Doctoral Programmes</a>
+  <a>Postdoctoral Fellowships</a>
+</header>
+
+<div id="logos">
+  <img src="FaVU_zkratka_cernobile_CZ.png" alt="FFA Logo">
+  <img src="FFA_abbreviation_black_white_EN.png" alt="FFA Logo">
+</div>
+
+<div class="home">
+  <h1>FFA Art Research Portal</h1>
+  <app-research-list></app-research-list>
+</div>
   `,
   styles: [`
     .home {

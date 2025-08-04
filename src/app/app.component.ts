6bb28dd0f77a390b54815f680ec3cab3d650b4e0
Andreas Gajdosik <andreas@gajdosik.org>
2025-08-04 14:48:09 +0530

    Add white background color

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,13 +9,7 @@ import { RouterOutlet } from '@angular/router';
     <main>
       <router-outlet></router-outlet>
     </main>
-  `,
-  styles: [`
-    main {
-      min-height: 100vh;
-      background: #f5f5f5;
-    }
-  `]
+  `
 })
 export class AppComponent {
   title = 'ffa-portal';

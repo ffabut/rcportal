3d08892dfc83e7d184d96491a9f940b161378f0a
Andreas Gajdosik <andreas@gajdosik.org>
2025-08-06 13:07:36 +0530

    make links to open in new tab

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,14 +8,14 @@ import { ResearchListComponent } from '../../components/research-list/research-l
   imports: [CommonModule, ResearchListComponent],
   template: `
 <header>
-  <a href="https://favu.vut.cz">FFA</a>
-  <a href="https://tinyurl.com/FFA-Organigram">Organigram</a>
-  <a href="https://favu.vut.cz">Research</a>
-  <a href="https://favu.vut.cz">Publishing</a>
-  <a href="https://favu.vut.cz">Gallery</a>
-  <a href="https://www.favu.vut.cz/en/students/programmes?data%5Bts%5D=N&data%5Bfs%5D=&data%5Brok%5D=2024&data%5Bjv%5D=">MA's in Fine Arts and Design</a>
-  <a href="https://www.favu.vut.cz/en/phd">Doctoral Programmes</a>
-  <a href="https://www.favu.vut.cz/en/post-mag-post-doc">Postdoctoral Fellowships</a>
+  <a href="https://favu.vut.cz" target="_blank">FFA</a>
+  <a href="https://tinyurl.com/FFA-Organigram" target="_blank">Organigram</a>
+  <a href="https://favu.vut.cz" target="_blank">Research</a>
+  <a href="https://favu.vut.cz" target="_blank">Publishing</a>
+  <a href="https://favu.vut.cz" target="_blank">Gallery</a>
+  <a href="https://www.favu.vut.cz/en/students/programmes?data%5Bts%5D=N&data%5Bfs%5D=&data%5Brok%5D=2024&data%5Bjv%5D=" target="_blank">MA's in Fine Arts and Design</a>
+  <a href="https://www.favu.vut.cz/en/phd" target="_blank">Doctoral Programmes</a>
+  <a href="https://www.favu.vut.cz/en/post-mag-post-doc" target="_blank">Postdoctoral Fellowships</a>
 </header>
 
 <div class="logos">

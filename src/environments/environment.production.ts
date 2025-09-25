3802f193b1c7a9c51ebffdb1a563da82d1983f46
Andreas Gajdosik <andreas@gajdosik.org>
2025-09-25 13:39:15 +0200

    Use rcmap proxy provided by Casper, thanks!

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   envFile: 'environment.production.ts',
-  apiBaseUrl: 'https://www.researchcatalogue.net/api'
-}; 
+  //apiBaseUrl: 'https://www.researchcatalogue.net/api'
+  apiBaseUrl: 'https://rcdata.org/api' // CORS proxy provided by Casper, huge thanks!
+};

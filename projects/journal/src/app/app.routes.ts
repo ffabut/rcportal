6b4164879724c50f28b0b299638b300de6f343ae
Andreas Gajdosik <andreas@gajdosik.org>
2025-11-20 14:35:18 +0100

    Add /about and /for_authors to router

--- projects/journal/src/app/app.routes.ts
+++ projects/journal/src/app/app.routes.ts
@@ -1,11 +1,23 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ForAuthorsComponent } from './pages/for_authors/for_authors.component';
 
 export const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
     component: HomeComponent
+  },
+  {
+    path: 'about',
+    pathMatch: 'full',
+    component: AboutComponent
+  },
+  {
+    path: 'for_authors',
+    pathMatch: 'full',
+    component: ForAuthorsComponent
   },
   {
     path: 'id/:articleId',

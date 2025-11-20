9dae28a4f3e93bacebf922ae3acb97916ee96c36
Andreas Gajdosik <andreas@gajdosik.org>
2025-11-20 10:25:40 +0100

    Fix the styling of the Journal header and logobar

--- projects/journal/src/app/pages/home/home.component.ts
+++ projects/journal/src/app/pages/home/home.component.ts
@@ -7,6 +7,7 @@ import { FooterComponent } from '../../../../../../shared/components/footer.comp
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule, JournalListComponent, FooterComponent],
+  styleUrl: './home.styles.scss',
   template: `
 <header>
   <a href="./">Home</a>
@@ -14,13 +15,20 @@ import { FooterComponent } from '../../../../../../shared/components/footer.comp
   <a href="for_authors">For authors</a>
 </header>
 
-  <h1>FFA Research in Art and Design</h1>
-  <div class="subtitle">ISSN: 1234-5678</div>
-
-<div class="logos">
-  <img src="FFA_black_white_EN.png" alt="FFA Logo">
-  <img src="FFA_abbreviation_black_white_EN.png" alt="FFA Logo">
+<div class="logobar">
+  <div class="logos">
+    <img src="FFA_black_white_EN.png" alt="FFA Logo">
+    <img src="FFA_abbreviation_black_white_EN.png" alt="FFA Logo">
+  </div>
+  <div class="titles">
+      <h1>FFA Research in Art and Design</h1>
+      <div class="subtitle">ISSN: 1993-2025</div>
+  </div>
 </div>
+
+
+
+
 
 <app-journal-list></app-journal-list>
 <app-footer></app-footer>

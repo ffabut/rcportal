354809794e3976859a77d71ea8648efbdd80b932
Andreas Gajdosik <andreas@gajdosik.org>
2025-11-20 14:34:46 +0100

    Init for_authors page

---
+++ projects/journal/src/app/pages/for_authors/for_authors.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from '../../../../../../shared/components/footer.component'
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, FooterComponent],
+  styleUrl: './for_authors.styles.scss',
+  template: `
+<header>
+  <a href="./">Home</a>
+  <a href="about">About</a>
+  <a href="for_authors">For authors</a>
+</header>
+
+<div class="logobar">
+  <div class="logos">
+    <img src="FFA_black_white_EN.png" alt="FFA Logo">
+    <img src="FFA_abbreviation_black_white_EN.png" alt="FFA Logo">
+  </div>
+  <div class="titles">
+      <h1>FFA Research in Art and Design</h1>
+      <div class="subtitle">ISSN: 1993-2025</div>
+  </div>
+</div>
+
+<app-footer></app-footer>
+`})
+export class ForAuthorsComponent {} 

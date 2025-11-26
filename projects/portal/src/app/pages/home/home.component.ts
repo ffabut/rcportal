5176eff393a103ef65861faae10211c460dbda35
Andreas Gajdosik <andreas@gajdosik.org>
2025-11-26 11:15:39 +0100

    Fix #15 linkovat na anglickou verzi galerie

--- projects/portal/src/app/pages/home/home.component.ts
+++ projects/portal/src/app/pages/home/home.component.ts
@@ -11,7 +11,7 @@ import { FooterComponent } from '../../components/footer.component'
 <header>
   <a href="https://favu.vut.cz/en/" target="_blank">FFA</a>
   <a href="https://favu.vut.cz/en/publishing" target="_blank">FFA Publishing</a>
-  <a href="https://galerie.favu.vut.cz" target="_blank">FFA Gallery</a>
+  <a href="https://galerie.favu.vut.cz/en" target="_blank">FFA Gallery</a>
   <a href="https://www.favu.vut.cz/en/international/degree-en" target="_blank">FAAD</a>
   <a href="https://www.favu.vut.cz/en/phd" target="_blank">Doctorate</a>
   <a href="https://www.favu.vut.cz/en/post-mag-post-doc" target="_blank">Postdoctoral Fellowships</a>

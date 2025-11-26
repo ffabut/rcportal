0501a7faa4ecb38ee37fe18ba7dda37839aaf890
Andreas Gajdosik <andreas@gajdosik.org>
2025-11-26 14:04:25 +0100

    Fix #19 set default page title on every page

--- projects/portal/src/app/app.routes.ts
+++ projects/portal/src/app/app.routes.ts
@@ -4,15 +4,16 @@ import { HomeComponent } from './pages/home/home.component';
 export const routes: Routes = [
   {
     path: '',
+    title: 'Faculty of Fine Arts BUT | Art Research Portal',
     pathMatch: 'full',
     component: HomeComponent
   },
   {
     path: 'id/:articleId',
+    title: 'FFARAD | Exposition Detail',
     loadComponent: () =>
       import('./pages/detail/detail.component')
         .then(m => m.ArticleDetailComponent),
-    title: 'Exposition Detail'
   },
   // fallback
   { 

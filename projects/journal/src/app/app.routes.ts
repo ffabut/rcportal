54aaa82cf15b4933c7ad7447b299770cd93cda46
Andreas Gajdosik <andreas@gajdosik.org>
2025-11-26 13:53:15 +0100

    Set default titles for Journal

--- projects/journal/src/app/app.routes.ts
+++ projects/journal/src/app/app.routes.ts
@@ -6,25 +6,28 @@ import { ForAuthorsComponent } from './pages/for_authors/for_authors.component';
 export const routes: Routes = [
   {
     path: '',
+    title: 'FFA Research in Art and Design | Journal',
     pathMatch: 'full',
     component: HomeComponent
   },
   {
     path: 'about',
+    title: 'FFARAD | About Journal',
     pathMatch: 'full',
     component: AboutComponent
   },
   {
     path: 'for_authors',
+    title: 'FFARAD | For Authors',
     pathMatch: 'full',
     component: ForAuthorsComponent
   },
   {
     path: 'id/:articleId',
+    title: 'FFARAD | Exposition Detail',
     loadComponent: () =>
       import('./pages/detail/detail.component')
         .then(m => m.ArticleDetailComponent),
-    title: 'Exposition Detail'
   },
   // fallback
   { 
